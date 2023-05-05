f9b40f2f7eb9a51139c80c80035313b46556f403
Gr3su <speedygoo67@googlemail.com>
2023-05-05 17:57:13 +0200

    -Dialog fertig

--- container/ueb16/AutomatDialog.java
+++ container/ueb16/AutomatDialog.java
@@ -13,7 +13,36 @@ import java.util.Scanner;
  */
 public class AutomatDialog {
     //Prompts
-    private static final String AUSGABE_HAUPTMENUE = "";
+    private static final String AUSGABE_HAUPTMENUE          = "0 - Dialog beenden\n" +
+                                                                "1 - Automat erstellen\n" +
+                                                                "2 - Automaten ausgeben\n" +
+                                                                "3 - Automat fuellen\n" +
+                                                                "4 - Automat leeren\n";
+    private static final String EINGABE_KAPAZITAET          = "Die Kapazitaet des Automaten:\n";
+    private static final String EINGABE_AUTOMAT_GETRAENK    = "Bitte waehle aus folgender Beschraenkung an " +
+                                                                "Getraenken aus:\n" +
+                                                                "0 - Getraenke\n" +
+                                                                "1 - Alkoholische Getraenke\n" +
+                                                                "2 - Alkoholfreie Getraenke\n" +
+                                                                "3 - Wasser\n" +
+                                                                "4 - Softdrink\n" +
+                                                                "5 - Bier\n" +
+                                                                "6 - Wein\n" +
+                                                                "7 - Rotwein\n" +
+                                                                "8 - Weisswein";
+    private static final String EINGABE_GETRANK_AUSWAHL     = "Bitte waehle ein folgendes Getraenk aus:\n" +
+                                                                "0 - Wasser\n" +
+                                                                "1 - Softdrink\n" +
+                                                                "2 - Bier\n" +
+                                                                "3 - Wein\n" +
+                                                                "4 - Rotwein\n" +
+                                                                "5 - Weisswein";
+    private static final String EINGABE_AUTOMATEN_AUSWAHL = "Bitte einen Automaten auswaehlen:";
+
+    //Errors
+    private static final String ERROR_KEIN_BYTE     = "Eingegebene Zeichen sind kein byte.";
+    private static final String ERROR_KEIN_FLOAT    = "Eingegebene Zeichen sind kein float.";
+    private static final String ERROR_KEIN_INT      = "Eingegebene Zeichen sind kein int.";
 
     //Attributes
     private Scanner scanner;
@@ -21,32 +50,163 @@ public class AutomatDialog {
 
     private AutomatDialog(){
         this.scanner = new Scanner(System.in);
+        automaten = new ArrayList<>();
     }
 
     public void start(){
+        byte option;
+
         while(true){
+            option = -1;
+
             try{
-                System.out.println(AUSGABE_HAUPTMENUE);
+                option = readByte(AUSGABE_HAUPTMENUE);
+                DialogException.validateMenueOption(option);
+
+                switch(option){
+                    case 0:
+                        return;
+
+                    case 1:
+                        automatErstellen();
+                        break;
+
+                    case 2:
+                        System.out.println(automatenAuswahl());
+                        break;
+
+                    case 3:
+                        automatFuellen();
+                        break;
+
+                    case 4:
+                        System.out.println(automatenAuswahl().flascheAusgeben());
+                        break;
+                }
             }
             catch(GetraenkeautomatException e){
                 System.err.println("Fehler in Getraenkeautomat:");
-                e.printStackTrace();
+                System.err.println(e.getMessage());
             }
             catch(DialogException e){
                 System.err.println("Fehler in Dialog:");
-                e.printStackTrace();
+                System.err.println(e.getMessage());
+            }
+            catch(GetraenkeException e){
+                System.err.println("Fehler in Getraenk-Erstellung:");
+                System.err.println(e.getMessage());
             }
             catch(Exception e){
+                System.err.println(e.getMessage());
                 e.printStackTrace();
             }
         }
     }
 
-    private int leseInt(String prompt){
+    private void automatErstellen(){
+        int kapazitaet = readInt(EINGABE_KAPAZITAET);
+        byte getraenk = readByte(EINGABE_AUTOMAT_GETRAENK);
+
+        switch (getraenk){
+            case 0:
+                automaten.add(new Getraenkeautomat<Getraenk>(kapazitaet));
+                break;
+
+            case 1:
+                automaten.add(new Getraenkeautomat<AlkoholischesGetraenk>(kapazitaet));
+                break;
+
+            case 2:
+                automaten.add(new Getraenkeautomat<AlkoholfreiesGetraenk>(kapazitaet));
+                break;
+
+            case 3:
+                automaten.add(new Getraenkeautomat<Wasser>(kapazitaet));
+                break;
+
+            case 4:
+                automaten.add(new Getraenkeautomat<Softdrink>(kapazitaet));
+                break;
+
+            case 5:
+                automaten.add(new Getraenkeautomat<Bier>(kapazitaet));
+                break;
+
+            case 6:
+                automaten.add(new Getraenkeautomat<Wein>(kapazitaet));
+                break;
+
+            case 7:
+                automaten.add(new Getraenkeautomat<Rotwein>(kapazitaet));
+                break;
+
+            case 8:
+                automaten.add(new Getraenkeautomat<Weisswein>(kapazitaet));
+                break;
+
+
+        }
+    }
+
+    public Getraenkeautomat automatenAuswahl(){
+        for(int i = 0; i < automaten.size(); i++){
+            System.out.println(i + " : " + automaten.get(i) + "\n");
+        }
+
+        int tmp = readInt(EINGABE_AUTOMATEN_AUSWAHL);
+        DialogException.validateAutomatSelection(automaten.size(), tmp);
+
+        return automaten.get(tmp);
+    }
+
+    private void automatFuellen(){
+        Getraenkeautomat automat = automatenAuswahl();
+        int getraenkeWahl = readInt(EINGABE_GETRANK_AUSWAHL);
+        DialogException.validateGetraenkBeiFlasche(getraenkeWahl);
+
+        String name = readString("Name des Getraenks:");
+
+        if(getraenkeWahl < 2){
+            String hersteller = readString("Name des Herstellers:");
+
+            if(getraenkeWahl == 0){
+                String quelle = readString("Wasserquelle:");
+                automat.flascheEinlegen(new Flasche<Wasser>(new Wasser(quelle, hersteller, name)));
+            }
+            else{
+                float zuckergehalt = readFloat("Zuckergehalt:");
+                automat.flascheEinlegen(new Flasche<Softdrink>(new Softdrink(zuckergehalt, hersteller, name)));
+            }
+        }
+
+        else{
+            float alkoholGehalt = readFloat("Alkoholgehalt:");
+
+            if(getraenkeWahl == 2){
+                String brauerei = readString("Name der Brauerei:");
+                automat.flascheEinlegen(new Flasche<Bier>(new Bier(brauerei, alkoholGehalt, name)));
+            }
+            else{
+                String weingut = readString("Weingut:");
+
+                if(getraenkeWahl == 3){
+                    automat.flascheEinlegen(new Flasche<Wein>(weingut, alkoholGehalt, name));
+                }
+                else if (getraenkeWahl == 4) {
+                    automat.flascheEinlegen(new Flasche<Rotwein>(weingut, alkoholGehalt, name));
+                }
+                else if (getraenkeWahl == 5) {
+                    automat.flascheEinlegen(new Flasche<Weisswein>(weingut, alkoholGehalt, name));
+                }
+            }
+        }
+    }
+
+    private int readInt(String prompt){
         System.out.println(prompt);
 
         while(!scanner.hasNextInt()){
-            System.err.println(ErrorMessages.KEIN_BYTE.getMessage());
+            System.err.println(ERROR_KEIN_INT);
             scanner.nextLine();
         }
 
@@ -56,11 +216,11 @@ public class AutomatDialog {
         return tmp;
     }
 
-    private byte leseByte(String prompt){
+    private byte readByte(String prompt){
         System.out.println(prompt);
 
         while(!scanner.hasNextByte()){
-            System.err.println(ErrorMessages.KEIN_BYTE.getMessage());
+            System.err.println(ERROR_KEIN_BYTE);
             scanner.nextLine();
         }
 
@@ -70,7 +230,21 @@ public class AutomatDialog {
         return tmp;
     }
 
-    private String leseString(String prompt){
+    private float readFloat(String prompt){
+        System.out.println(prompt);
+
+        while(!scanner.hasNextFloat()){
+            System.err.println(ERROR_KEIN_FLOAT);
+            scanner.nextLine();
+        }
+
+        float tmp = scanner.nextFloat();
+        scanner.nextLine();
+
+        return tmp;
+    }
+
+    private String readString(String prompt){
         System.out.println(prompt);
 
         return scanner.nextLine();
--- container/ueb16/DialogException.java
+++ container/ueb16/DialogException.java
@@ -7,4 +7,47 @@ package ueb16;
  * @version 05.05.2023 / 13:00Uhr
  */
 public class DialogException extends RuntimeException{
+    //Attributes
+    private static final int MAX_OPTION = 4;
+
+    //Prompts
+    private static final String ERROR_OPTIONSBEREICH       = "Die Option muss zwischen 0 und " + MAX_OPTION + "liegen.";
+    private static final String ERROR_AUTOMATENAUSWAHL      = "Automat konnte nicht gefunden werden.";
+    private static final String ERROR_GETRAENK_BEI_AUTOMAT  = "Die Getraenkeauswahl muss zwischen 0 und 8 liegen.";
+    private static final String ERROR_GETRAENK_BEI_FLASCHE  = "Die Getraenkeauswahl muss zwischen 0 und 5 liegen.";
+
+    public DialogException(){
+        super();
+    }
+    public DialogException(String msg){
+        super(msg);
+    }
+
+    public static void validateMenueOption(byte option){
+        if(option < 0 ||
+            option > MAX_OPTION){
+            throw new DialogException(ERROR_OPTIONSBEREICH);
+        }
+    }
+
+    public static void validateAutomatSelection(int size, int selection){
+        if(selection < 0 ||
+            selection >= size){
+            throw new DialogException(ERROR_AUTOMATENAUSWAHL);
+        }
+    }
+
+    public static void validateGetraenkBeiAutomat(int opt){
+        if(opt < 0 ||
+            opt > 8){
+            throw new DialogException(ERROR_GETRAENK_BEI_AUTOMAT);
+        }
+    }
+
+    public static void validateGetraenkBeiFlasche(int opt){
+        if(opt < 0 ||
+                opt > 5){
+            throw new DialogException(ERROR_GETRAENK_BEI_FLASCHE);
+        }
+    }
 }
