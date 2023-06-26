6c17511244f45758864085dba9b769a992cf2b63
Gr3su <speedygoo67@googlemail.com>
2023-06-26 22:54:39 +0200

    Tests ueberarbeitet

--- container/ueb20/Aufgabe1/ProducerTest.java
+++ container/ueb20/Aufgabe1/ProducerTest.java
@@ -1,6 +1,5 @@
 package ueb20.Aufgabe1;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,13 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ProducerTest {
     private Producer producer;
 
-    @BeforeEach
-    void setUp() {
-        producer = new Producer();
-    }
-
     @Test
     void producer_produce_shouldReturnNumberWithinRange_0_1001() {
+        producer = new Producer();
         int value = producer.produce();
         assertTrue(value >= 0 && value < 1001);
     }
