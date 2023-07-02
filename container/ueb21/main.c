53eb38ee51bb7f601f5f976401d39be44e2e5fa5
Gr3su <speedygoo67@googlemail.com>
2023-07-02 17:30:33 +0200

    c Aufgabe aufgeraeumt und korrigiert

--- container/ueb21/main.c
+++ container/ueb21/main.c
@@ -5,206 +5,90 @@
  * @author Tim Mueller / Yannick Gross
  * @version 28.06.23 / 6:00Uhr
  */
-
-void verdoppeln(int x) {
-    x = x * 2;
-    printf("Innerhalb der Funktion: %d\n", x);
-}
+ int fibonacci(void);
+ int bruttoRechner(void);
+int minMaxFinder(void);
+int callByValue(void);
+int verdoppeln(int value);
 
 /**
  * Main Methode welche die Aufgaben 2-5 beinhaltet.
  */
 
 int main() {
-    double nettopreis, bruttopreis, skonto, mwst;
-    int auswahl, fibonacci;
-    int zahl = 5;
-
-    printf("\nBitte weahlen sie die Aufgabe aus:\n");
-
-    for (int i = 2; i <= 5; i++) {
-        printf("Aufgabe: %1i\n", i);
-    }
-    printf("Beenden: 6 \n");
-
-    scanf("%i", &auswahl);
-
-
-    /**
-    * Aufgabe 2 welche auf einem eingegebenen Nettopreis 20% Mehrwertsteuer aufrechnet und danach 2% Skonto abzieht.
-    */
-
-    if(auswahl == 2) {
-        // Eingabe des Nettopreises
-        printf("Geben Sie den Nettopreis in Euro ein: ");
-        scanf("%lf", &nettopreis);
-
-        // Berechnung der Mehrwertsteuer
-        mwst = 0.2 * nettopreis;
-
-        // Berechnung des Bruttopreises
-        bruttopreis = nettopreis + mwst;
-
-        // Berechnung des Skontos
-        skonto = 0.02 * bruttopreis;
-
-        // Ausgabe der Ergebnisse
-        printf("%-15s%15s%8.2f\n", "Nettopreis", "Euro",nettopreis);
-        printf("%-15s%15s%8.2f\n", "+ 20%MwSt", "Euro", mwst);
-        printf("%37s\n", "=====================================");
-        printf("%-15s%15s%8.2f\n","Bruttopreis", "Euro", bruttopreis);
-        printf("%-15s%15s%8.2f\n","- 2% Skonto", "Euro", skonto);
-        printf("%37s\n", "=====================================");
-        printf("%-15s%15s%8.2f\n", "Rechnungsbetrag","Euro", bruttopreis - skonto);
-
-        main();
-        return 0;
-
-    }
-
-    /**
-    * Aufgabe 3 welche eine n (anzahl) einließt und daraus die ersten Fibonacci Zahlen berechnet.
-    */
-
-    if(auswahl == 3) {
-        // Eingabe der Anzahl der Fibonacci-Zahlen
-        printf("Geben Sie die Anzahl der Fibonacci-Zahlen ein: ");
-        scanf("%d", &fibonacci);
-
-        // Überprüfen, ob n gültig ist
-        while (fibonacci <= 0) {
-            printf("Ungueltige Eingabe. Die Anzahl der Fibonacci-Zahlen muss groeßer als 0 sein.\n");
-
-            printf("Geben Sie die Anzahl der Fibonacci-Zahlen ein: ");
-            scanf("%d", &fibonacci);
+    while(1){
+        //Menue-Prompt
+        printf("\n%s\n%-3d : %s\n","Bitte waehlen Sie aus:", 1, "Dialog beenden");
+        for(int i = 2; i < 6; i++){
+            printf("%-3d : %7s %d\n", i, "Aufgabe", i);
         }
 
-        // Berechnung und Ausgabe der Fibonacci-Zahlen
-        int fib1 = 0;   // Erstes Fibonacci-Zahl
-        int fib2 = 1;   // Zweite Fibonacci-Zahl
-
-        printf("Die ersten %d Fibonacci-Zahlen sind:\n", fibonacci);
-        printf("%d\n", fib1);   // Ausgabe der ersten Fibonacci-Zahl
-
-        if (fibonacci > 1) {
-            printf("%d\n", fib2);   // Ausgabe der zweiten Fibonacci-Zahl
-
-            // Berechnung und Ausgabe der restlichen Fibonacci-Zahlen
-            for (int i = 3; i <= fibonacci; i++) {
-                int fib = fib1 + fib2;
-                printf("%d\n", fib);
-
-                fib1 = fib2;
-                fib2 = fib;
+        //Eingabe-Ueberpruefung
+        int errorCode = 0;
+        int menuaWahl = 0;
+        while (!errorCode) {
+            printf("%s\n","Geben Sie die Aufgabennummer ein: ");
+            errorCode = scanf("%d", &menuaWahl);
+            if(errorCode < 1 ||
+                menuaWahl < 1 ||
+                menuaWahl > 5){
+                printf("%s\n", "Bitte eine Ganzzahl zwischen 1 und 5 eingeben:");
+                char dummy;
+                scanf("%c", &dummy);
             }
         }
-        main();
-        return 0;
-    }
+        printf("\n");
 
-    /**
-    * Aufgabe 4 gibt die maximalen und minimalen Werte der Typen char, short, int, unsigned char, unsigned
-    *  short und unsigned int aus.
-    */
+        switch (menuaWahl) {
+            case 1:
+                return 0;
 
-    if(auswahl == 4) {
+            case 2:
+                bruttoRechner();
+                break;
 
-        printf("maximale und minamale Werte manuell eingegeben: \n\n");
+            case 3:
+                fibonacci();
+                break;
 
+            case 4:
+                minMaxFinder();
+                break;
 
-        printf("Datentyp          Min-Wert             Max-Wert\n");
-        printf("====================================================\n");
+            case 5:
+                callByValue();
+                break;
 
-        // char
-        printf("char              %2d        %12d\n", -128, 127);
-
-        // short
-        printf("short             %2d        %12d\n", -32768, 32767);
-
-        // int
-        printf("int               %2d        %12d\n", -2147483648, 2147483647);
-
-        // unsigned char
-        printf("unsigned char     %2u        %17u\n", 0, 255);
-
-        // unsigned short
-        printf("unsigned short    %2u        %17u\n", 0, 65535);
-
-        // unsigned int
-        printf("unsigned int      %2u        %17u\n\n", 0, 4294967295);
-
-        printf("maximale und minamale Werte mit der Bibliothek limits.h: \n\n");
-
-
-        printf("Datentyp          Min-Wert             Max-Wert\n");
-        printf("====================================================\n");
-
-        // char
-        printf("char              %2d        %12d\n", CHAR_MIN, CHAR_MAX);
-
-        // short
-        printf("short             %2d        %12d\n", SHRT_MIN, SHRT_MAX);
-
-        // int
-        printf("int               %2d        %12d\n", INT_MIN, INT_MAX);
-
-        // unsigned char
-        printf("unsigned char     %2d        %17u\n", 0, UCHAR_MAX);
-
-        // unsigned short
-        printf("unsigned short    %2d        %17u\n", 0, USHRT_MAX);
-
-        // unsigned int
-        printf("unsigned int      %2d        %17u\n", 0, UINT_MAX);
-
-        main();
-        return 0;
-    }
-
-    /**
-    * Aufgabe 5 beschreibt mit hilfe einer methode das call-by-value-Prinzip.
-    */
-
-    if(auswahl == 5) {
-        printf("Vor dem Aufruf der Funktion: %d\n", zahl);
-
-        verdoppeln(zahl);
-
-        printf("Nach dem Aufruf der Funktion: %d\n", zahl);
-
-        main();
-        return 0;
-
-    }
-
-    if(auswahl == 6) {
-        return 0;
-    }
-
-    else{
-        printf("Bitte geben sie eine Zahl von 2-6 ein", zahl);
-
-        main();
-        return 0;
+            default:
+                printf("%s\n","Unerwarteter Fehler");
+                break;
+        }
     }
 
 }
 
-int fibonacci(){
-    return 0;
-}
-
-/*
+/**
  * Aufgabe 2:
  * Berechnet zu einem eingegebenen Preis 20% Mehrwertsteuer dazu, zieht dann 2% Skonto ab und gibt jeden
  * Rechnungsschritt formatiert aus.
+ *
+ * @return 0
  */
-int bruttoRechner(){
+int bruttoRechner(void){
     double nettopreis, bruttopreis, mwst, skonto;
+    int errorCode = 0;
 
     // Eingabe des Nettopreises
-    printf("Geben Sie den Nettopreis in Euro ein: ");
-    scanf("%lf", &nettopreis);
+    while (!errorCode) {
+        printf("%s\n","Geben Sie den Nettopreis in Euro ein: ");
+        errorCode = scanf("%lf", &nettopreis);
+        if(errorCode < 1){
+            printf("%s\n", "Bitte eine Gleitkommazahl eingeben:");
+            char trash;
+            scanf("%c", &trash);
+        }
+    }
+
 
     // Berechnung der Mehrwertsteuer
     mwst = 0.2 * nettopreis;
@@ -223,4 +107,128 @@ int bruttoRechner(){
     printf("%-15s%15s%8.2f\n","-2% Skonto", "Euro", skonto);
     printf("%37s\n", "=====================================");
     printf("%-15s%15s%8.2f\n", "Rechnungsbetrag","Euro", bruttopreis - skonto);
+
+    return 0;
+}
+
+/**
+ * Aufgabe 3
+ * Gibt die Fibonacci-Folge aus bis zum gewuenschten Index.
+ *
+ * @return 0
+ */
+int fibonacci(void){
+    int fibonacciEnde;
+    int errorCode = 0;
+    int fibA = 0;
+    int fibB = 1;
+    int fibC;
+
+    printf("%s\n", "Bitte gebe die Zahl ein bis wohin die Fibonacci-Folge abgebildet werden soll:");
+
+    //Fragt nach einer oberen Schranke bis eine Ganzzahl eingegeben wurde
+    while (!errorCode){
+        errorCode = scanf("%d", &fibonacciEnde);
+        if(errorCode < 1){
+            printf("%s\n", "Bitte gebe eine Ganzzahl ein:");
+            char trash;
+            scanf("%c", &trash);
+        }
+    }
+
+    //Gibt die Folge aus und berechnet sie iterativ, fortlaufend weiter
+    for(int i = 0; i <= fibonacciEnde; i++){
+        printf("%d - ", fibA);
+        fibC = fibA + fibB;
+        fibA = fibB;
+        fibB = fibC;
+    }
+
+    return 0;
+}
+
+/**
+ * Aufgabe 4
+ * Findet die minimalen und maximalen Werte die (unsigned) char, (unsigned) short und (unsigned) int annehmen koennen
+ * und printet sie aus. Mit dem Vergleich aus der LIMITS Library.
+ *
+ * @return 0
+ */
+int minMaxFinder(void){
+    printf("%-15s%15s%15s\n","Daten-Typ","Min-Wert","Max-Wert");
+
+    char charMax = 0;
+    do{
+        charMax++;
+    }
+    while(charMax > 0);
+    printf("%-15s%15d","char", charMax);
+    charMax--;
+    printf("%15d\n",charMax);
+
+
+    short shortMax = 0;
+    do{
+        shortMax++;
+    }
+    while(shortMax > 0);
+    printf("%-15s%15d","short", shortMax);
+    shortMax--;
+    printf("%15d\n", shortMax);
+
+
+    int intMax = 0;
+    do{
+        intMax++;
+    }
+    while(intMax > 0);
+    printf("%-15s%15d","int", intMax);
+    intMax--;
+    printf("%15d\n",intMax);
+
+
+    unsigned char unsignedCharMax = 0;
+    printf("%-15s%15d","unsigned char", unsignedCharMax);
+    unsignedCharMax--;
+    printf("%15d\n",unsignedCharMax);
+
+
+    unsigned short unsignedShortMax = 0;
+    printf("%-15s%15d","unsigned short", unsignedShortMax);
+    unsignedShortMax--;
+    printf("%15d\n",unsignedShortMax);
+
+
+    unsigned int unsignedIntMax = 0;
+    printf("%-15s%15d","unsigned int", unsignedIntMax);
+    unsignedIntMax--;
+    printf("%15d\n", unsignedIntMax);
+
+    printf("\n%s\n","Aus limits-library:");
+    printf("%-15s%15s%15s\n","Daten-Typ","Min-Wert","Max-Wert");
+    printf("%-15s%15d%15d\n","char", CHAR_MIN, CHAR_MAX);
+    printf("%-15s%15d%15d\n","short", SHRT_MIN, SHRT_MAX);
+    printf("%-15s%15d%15d\n","int", INT_MIN, INT_MAX);
+    printf("%-15s%15d%15d\n","unsigned char", 0, UCHAR_MAX);
+    printf("%-15s%15d%15d\n","unsgined short", 0, USHRT_MAX);
+    printf("%-15s%15d%15d\n","unsgined int", 0, UINT_MAX);
+
+    return 0;
+}
+
+int callByValue(void){
+    short value = 2;
+    printf("%-50s%2d\n","Wert der Variable vor Funktionsaufruf:", value);
+    verdoppeln(value);
+    printf("%-50s%2d\n","Wert der Variable nach Funktionsaufruf:", value);
+
+    return 0;
+}
+
+int verdoppeln(int value){
+    printf("%-50s%2d\n","Wert der Variable in der Funktion:", value);
+    value = value * 2;
+    printf("%-50s%2d\n","Wert der Variable in der Funktion, nach Verdopplung:", value);
+
+    return 0;
 }
